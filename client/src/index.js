9a339d6c9aba65037a246d0513ce4d820e9b0a8f
niyas <niyaskn01@gmail.com>
2024-02-04 19:39:17 +0530

    changes in clientside

--- client/src/index.js
+++ client/src/index.js
@@ -9,16 +9,10 @@ import { theme } from './styles/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <BrowserRouter>
       <ThemeProvider theme={theme}>
         <App />
       </ThemeProvider>
     </BrowserRouter>
-  </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
